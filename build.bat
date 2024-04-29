9ba53f6b3fe4354fe2633e03834a3b37406510d3
xXxNIKIxXx <niklas@fuchs-kiel.de>
2024-04-29 09:46:34 +0200

    Finished build.bat

--- build.bat
+++ build.bat
@@ -19,6 +19,27 @@ del /S /Q *.spec
 echo Deleting the build folder...
 rd /s /q build
 
+set /p autostart="Do you want to move the Files in autostart? (YES/NO): "
+if /i "%autostart%"=="YES" (
+    cd ./dist
+    move *.* "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+    cd ..
+) else if /i "%autostart%"=="NO" (
+    rem
+) else (
+    echo Invalid input. Please type YES or NO.
+)
+
+
+set /p build_delete="Do you want to remove the build files? (YES/NO): "
+if /i "%build_delete%"=="YES" (
+    rd /s /q dist
+) else if /i "%build_delete%"=="NO" (
+    rem
+) else (
+    echo Invalid input. Please type YES or NO.
+)
+
 echo Cleanup completed.
 
 pause
